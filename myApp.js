b50bebabba31ad815d07760213b3b99a630cf992
Doominator-726 <nate.r.laing@gmail.com>
2025-02-16 18:10:34 -0500

    file send

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// #1
+// console.log("Hello World");
+
+// #2
+//app.get("/", function(req, res) {
+  //  res.send("Hello Express");
+//  });
+
+var abspath = __dirname + '/views/index.html';
 
 app.get("/", function(req, res) {
-    res.send("Hello Express");
-  });
-
-
+    res.sendFile(abspath);
+})
 
 
 
