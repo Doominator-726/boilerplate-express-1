0110ae58c5517029e66576b41c88a5e674bfb8ff
Doominator-726 <nate.r.laing@gmail.com>
2025-02-16 18:17:27 -0500

    Static

--- myApp.js
+++ myApp.js
@@ -9,13 +9,17 @@ let app = express();
   //  res.send("Hello Express");
 //  });
 
-var abspath = __dirname + '/views/index.html';
+// #3
+//var abspath = __dirname + '/views/index.html';
 
-app.get("/", function(req, res) {
-    res.sendFile(abspath);
-})
+//app.get("/", function(req, res) {
+    //res.sendFile(abspath);
+//})
 
+// # 4
 
+var abspath = __dirname + '/public';
+app.use(abspath, express.static());
 
 
 
