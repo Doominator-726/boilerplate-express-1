38f0464fecf382fc30813f8bb8a9011a20bf39c3
Doominator-726 <nate.r.laing@gmail.com>
2025-02-16 22:23:16 -0500

    time

--- myApp.js
+++ myApp.js
@@ -45,7 +45,16 @@ app.get("/json", function(req, res) {
     res.json({"message": message});
     
   });
-  
+
+// # 7
+
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+}
+)
 
 
 
